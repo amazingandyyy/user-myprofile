f1af1e26f32d83972c5d6a68deb30a073dcf15c1
Amazingandyyy <amazingandyyy@gmail.com>
2016-05-31 20:38:21 -0700

    update Mongo DB link
    update Mongo DB link

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
 
-const MONGOURL = 'mongodb://localhost/intro-to-auth';
+const MONGOURL = process.env.MONGODB_URI || 'mongodb://localhost/mongo-profile';
 
 mongoose.connect(MONGOURL, err => {
   console.log(err || `MongoDB connected to ${MONGOURL}`);
